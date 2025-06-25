c984eb0d7deaa38d56bdfd85f7e817355e83a638
esrabildikk <esrabildik752@gmail.com>
2025-06-25 09:44:36 +0300

    feat(feature) : update Login Screen

---
+++ AuthApp/feature/src/main/java/com/esrabildik/feature/component/LoadingLottie.kt
@@ -0,0 +1,25 @@
+package com.esrabildik.feature.component
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.Lottie
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
+
+
+@Composable
+fun LoadingLottie(resId:Int, height: Dp = 300.dp){
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(resId))
+    LottieAnimation(
+        composition = composition,
+        iterations = LottieConstants.IterateForever,
+        modifier = Modifier.height(height)
+    )
+}
--- AuthApp/feature/src/main/java/com/esrabildik/feature/ui/LoginScreen.kt
+++ AuthApp/feature/src/main/java/com/esrabildik/feature/ui/LoginScreen.kt
@@ -1,7 +1,7 @@
 package com.esrabildik.feature.ui
 
 
-import androidx.compose.foundation.Image
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -9,8 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Email
@@ -19,84 +17,121 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.esrabildik.feature.R
+import com.esrabildik.feature.component.LoadingLottie
+import com.esrabildik.feature.viewmodel.AuthViewModel
 
 
 @Composable
-fun LoginUI(){
+fun LoginUI(
+    viewModel: AuthViewModel = hiltViewModel()
+) {
+    val signInState by viewModel.uiState.collectAsState()
 
-       var email = remember { mutableStateOf("") }
-       var password = remember { mutableStateOf("") }
-
-       Column(modifier = Modifier
-              .fillMaxSize()
-              .padding(24.dp),
-              verticalArrangement = Arrangement.Center)
-       {
-              Text(
-                     text = "Login",
-                     fontSize = 32.sp,
-                     modifier = Modifier.padding(bottom = 24.dp)
-              )
-
-              CustomTextField(
-                     value = email.value,
-                     onValueChange = {email.value = it},
-                     label = "Email Address",
-                     placeholder = "Enter your Email",
-                     leadingIcon = Icons.Default.Email
-              )
-
-              Spacer(modifier = Modifier.height(17.dp))
-
-              CustomTextField(
-                     value = password.value,
-                     onValueChange = {password.value = it},
-                     label = "Password",
-                     placeholder = "Enter your Password",
-                     leadingIcon = Icons.Default.Email
-              )
-
-              Button(onClick = {}) {
-                     Icon(imageVector = Icons.Default.Done, contentDescription = null)
-                     Text("Login", Modifier.padding(start = 10.dp))}
-       }
-
-}
+    var email = remember { mutableStateOf("") }
+    var password = remember { mutableStateOf("") }
+    val context = LocalContext.current
 
 
+    LaunchedEffect(signInState.error) {
+        signInState.error?.let { error ->
+            Toast.makeText(context, error, Toast.LENGTH_SHORT).show()
+        }
 
+    }
 
- @Composable
-fun CustomTextField(
-       value  : String,
-       onValueChange : (String) -> Unit,
-       label : String,
-       placeholder: String,
-       leadingIcon : ImageVector? = null,
-       modifier: Modifier = Modifier
-){
-       OutlinedTextField(
-               value = value,
-               leadingIcon = leadingIcon?.let {
-                      { Icon(imageVector = it, contentDescription = null) }
-               },
-               onValueChange = onValueChange,
-               label = { Text(label)},
-               placeholder = { Text(placeholder) },
-              modifier = modifier.fillMaxWidth()
+    if (signInState.isLoading) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            LoadingLottie(resId = R.raw.loading_anim)
+        }
+    } else {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp),
+            verticalArrangement = Arrangement.Center
         )
+        {
+            Text(
+                text = "Login",
+                fontSize = 32.sp,
+                modifier = Modifier.padding(bottom = 24.dp)
+            )
+
+            CustomTextField(
+                value = email.value,
+                onValueChange = { email.value = it },
+                label = "Email Address",
+                placeholder = "Enter your Email",
+                leadingIcon = Icons.Default.Email
+            )
+
+            Spacer(modifier = Modifier.height(17.dp))
+
+            CustomTextField(
+                value = password.value,
+                onValueChange = { password.value = it },
+                label = "Password",
+                placeholder = "Enter your Password",
+                leadingIcon = Icons.Default.Email
+            )
+
+            Button(onClick = {
+                viewModel.signIn(email.value, password.value)
+            }) {
+                Icon(imageVector = Icons.Default.Done, contentDescription = null)
+                Text("Login", Modifier.padding(start = 10.dp))
+            }
+        }
+    }
+
+
 }
 
+
+@Composable
+fun CustomTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    placeholder: String,
+    leadingIcon: ImageVector? = null,
+    modifier: Modifier = Modifier
+) {
+    OutlinedTextField(
+        value = value,
+        leadingIcon = leadingIcon?.let {
+            { Icon(imageVector = it, contentDescription = null) }
+        },
+        onValueChange = onValueChange,
+        label = { Text(label) },
+        placeholder = { Text(placeholder) },
+        modifier = modifier.fillMaxWidth()
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginPreview() {
+    CustomTextField("value",{},"label","placeholder")
+}
 
 
 
