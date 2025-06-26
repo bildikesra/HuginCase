a807cb2eb92d03f011b5d52feda83f5cbf872b1c
esrabildikk <esrabildik752@gmail.com>
2025-06-26 11:39:56 +0300

    feat : RegisterViewModel created

---
+++ AuthApp/feature/src/main/java/com/esrabildik/feature/register/viewmodel/RegisterViewModel.kt
@@ -0,0 +1,9 @@
+package com.esrabildik.feature.register.viewmodel
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class RegisterViewModel @Inject constructor(
+
+) : ViewModel() {
+}
