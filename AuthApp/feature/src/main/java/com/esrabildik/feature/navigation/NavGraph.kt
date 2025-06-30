6fd14cf0f4e26be83f9b447379e8acc8fa526190
esrabildikk <esrabildik752@gmail.com>
2025-06-30 09:24:47 +0300

    feat : navigation update

--- AuthApp/feature/src/main/java/com/esrabildik/feature/navigation/NavGraph.kt
+++ AuthApp/feature/src/main/java/com/esrabildik/feature/navigation/NavGraph.kt
@@ -1,7 +1,7 @@
 package com.esrabildik.feature.navigation
 
-sealed class NavGraph (val route : String){
+sealed class NavGraph (val route : String,val title : String){
 
-    object LoginScreen : NavGraph("login_screen")
-    object RegisterScreen : NavGraph("register_screen")
+    object LoginScreen : NavGraph("login_screen","Login")
+    object RegisterScreen : NavGraph("register_screen", "Register")
 }
--- AuthApp/feature/src/main/java/com/esrabildik/feature/navigation/NavHost.kt
+++ AuthApp/feature/src/main/java/com/esrabildik/feature/navigation/NavHost.kt
@@ -3,7 +3,8 @@ package com.esrabildik.feature.navigation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.core.content.pm.ShortcutInfoCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -11,23 +12,27 @@ import com.esrabildik.feature.login.LoginUI
 import com.esrabildik.feature.register.RegisterUI
 
 @Composable
-fun AppNavHost(){
+fun AppNavHost(navController: NavHostController) {
     Surface(color = MaterialTheme.colorScheme.background) {
 
-        val navController = rememberNavController()
+        NavHost(
+            navController = navController,
+            startDestination = NavGraph.LoginScreen.route
+        ) {
+            composable(route = NavGraph.LoginScreen.route) {
+                LoginUI(
+                    navigateToRegistry = { navController.navigate(NavGraph.RegisterScreen.route) },
+                )
+            }
 
-       NavHost(
-           navController = navController,
-           startDestination = NavGraph.LoginScreen.route
-       ){
-          composable(route = NavGraph.LoginScreen.route){
-              LoginUI(navController = navController)
-          }
+            composable(route = NavGraph.RegisterScreen.route) {
+                RegisterUI(
+                    navigationToLogin = { navController.navigate(NavGraph.LoginScreen.route)},
+                )
+            }
 
-           composable(route = NavGraph.RegisterScreen.route){
-               RegisterUI(onSuccess = {}, navController = navController )
-           }
-       }
+
+        }
 
     }
 }
