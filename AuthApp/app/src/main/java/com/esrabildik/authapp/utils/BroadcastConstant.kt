d5761a5b3668edb51bb40cf1a9c3236b807584a2
esrabildikk <esrabildik752@gmail.com>
2025-07-02 15:10:09 +0300

    feat : INTENT_ACTION added

--- AuthApp/app/src/main/java/com/esrabildik/authapp/utils/BroadcastConstant.kt
+++ AuthApp/app/src/main/java/com/esrabildik/authapp/utils/BroadcastConstant.kt
@@ -1,4 +1,4 @@
 package com.esrabildik.authapp.utils
 
 const val SERVICE_START_ACTION = "com.esrabildik.huginservice.START_SERVICE"
-const val BROADCAST_ACTION = "com.esrabildik.LOGIN_SUCCESS"
+const val INTENT_ACTION = "com.esrabildik.LOGIN_SUCCESS"
