cb8110700d19f22b32551209775656d2de804a21
esrabildikk <esrabildik752@gmail.com>
2025-07-02 15:58:34 +0300

    init module implementation

--- AppProduct/app/build.gradle.kts
+++ AppProduct/app/build.gradle.kts
@@ -41,6 +41,10 @@ android {
 
 dependencies {
 
+    implementation(project(":feature"))
+    implementation(project(":domain"))
+    implementation(project(":data"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
