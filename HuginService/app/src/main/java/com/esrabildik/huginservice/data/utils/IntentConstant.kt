03a03b55985bdff0d321b1b714deb972efb308fd
esrabildikk <esrabildik752@gmail.com>
2025-07-02 15:11:23 +0300

    feat : IntentConstant.kt created

---
+++ HuginService/app/src/main/java/com/esrabildik/huginservice/data/utils/IntentConstant.kt
@@ -0,0 +1,6 @@
+package com.esrabildik.huginservice.data.utils
+
+
+const val NOTIFICATION_ID = 1001
+const val CHANNEL_ID = "login_channel"
+const val INTENT_ACTION = "com.esrabildik.LOGIN_SUCCESS"
