8649e160fa2283260a42ac4c5427642f596876ab
esrabildikk <esrabildik752@gmail.com>
2025-07-02 15:10:46 +0300

    feat : delete BroadcastConstant.kt

--- HuginService/app/src/main/java/com/esrabildik/huginservice/data/utils/BroadcastConstant.kt
+++
@@ -1,7 +0,0 @@
-package com.esrabildik.huginservice.data.utils
-
-
-const val NOTIFICATION_ID = 1001
-const val CHANNEL_ID = "login_channel"
-const val BROADCAST_ACTION = "com.esrabildik.LOGIN_SUCCESS"
-const val LOGIN_SAVE_RESULT = "com.esrabildik.LOGIN_SAVE_RESULT"
