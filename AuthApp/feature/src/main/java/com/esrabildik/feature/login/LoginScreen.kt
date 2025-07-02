1857379317e39568c3cd3a4305da9147e14ea386
esrabildikk <esrabildik752@gmail.com>
2025-07-02 10:37:36 +0300

    feat : startService parameter was added

--- AuthApp/feature/src/main/java/com/esrabildik/feature/login/LoginScreen.kt
+++ AuthApp/feature/src/main/java/com/esrabildik/feature/login/LoginScreen.kt
@@ -1,9 +1,10 @@
 package com.esrabildik.feature.login
 
 
+
+import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
@@ -13,26 +14,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
-import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -44,25 +37,33 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.navigation.NavController
 import com.esrabildik.domain.model.UserRequest
+import com.esrabildik.domain.model.UserResponse
 import com.esrabildik.feature.R
 import com.esrabildik.feature.component.LoadingLottie
 import com.esrabildik.feature.login.event.LoginEvent
 import com.esrabildik.feature.login.viewmodel.LoginViewModel
-import com.esrabildik.feature.navigation.NavGraph
 
 
 @Composable
 fun LoginUI(
     viewModel: LoginViewModel = hiltViewModel(),
     navigateToRegistry: () -> Unit = {},
+    startService : (user : UserResponse) -> Unit
 ) {
 
     val signInState by viewModel.uiState.collectAsStateWithLifecycle()
     val password by viewModel.password
     val email by viewModel.email
     val context = LocalContext.current
+
+
+    LaunchedEffect(signInState.user) {
+        signInState.user?.let { user ->
+            Log.d("startService","user : ${user.email}")
+           startService(user)
+        }
+    }
 
     LaunchedEffect(signInState.error) {
         signInState.error?.let { error ->
@@ -78,7 +79,7 @@ fun LoginUI(
         ) {
             LoadingLottie(resId = R.raw.loading_anim)
         }
-    } else {
+    }
 
         Card(
             modifier = Modifier
@@ -111,7 +112,11 @@ fun LoginUI(
                 )
 
                 Spacer(modifier = Modifier.height(6.dp))
-                Divider(modifier = Modifier.fillMaxWidth(), thickness = 2.dp, color = Color.LightGray)
+                HorizontalDivider(
+                    modifier = Modifier.fillMaxWidth(),
+                    thickness = 2.dp,
+                    color = Color.LightGray
+                )
 
                 CustomButton(
                     text = "SIGN UP",
@@ -120,7 +125,6 @@ fun LoginUI(
                 )
 
             }
-        }
     }
 }
 
@@ -128,6 +132,7 @@ fun LoginUI(
 @Composable
 fun LoginEmailInput(
     email: String,
+
     onEmailChange: (String) -> Unit
 ) {
     Column(
@@ -218,6 +223,8 @@ fun CustomButton(
         }
     }
 }
+
+
 
 
 @Composable
