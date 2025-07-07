080e998e025b77bf5e754bba29d3559b33df3f8b
esrabildikk <esrabildik752@gmail.com>
2025-07-07 09:03:32 +0300

    feat : feature gradle update

--- AppProduct/feature/build.gradle.kts
+++ AppProduct/feature/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt.plugin)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -35,18 +36,42 @@ android {
 }
 
 dependencies {
-
     implementation(project(":domain"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.runtime.livedata)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+
+    // Compose lifecycle
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    
 
     // Hilt
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
+
+
+
+    // Datastore
+    implementation(libs.androidx.datastore.preferences)
+
+    implementation (libs.gson)
+
+    debugImplementation(libs.ui.tooling)
+
+    // Coil
+    implementation(libs.coil.compose)
 
 }
