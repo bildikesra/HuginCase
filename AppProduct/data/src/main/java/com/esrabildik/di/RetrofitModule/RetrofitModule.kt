588eb1a4da679f9bb72705af1922b9d811e598ec
esrabildikk <esrabildik752@gmail.com>
2025-07-07 09:02:03 +0300

    feat : retrofit module created

---
+++ AppProduct/data/src/main/java/com/esrabildik/di/RetrofitModule/RetrofitModule.kt
@@ -0,0 +1,38 @@
+package com.esrabildik.di.RetrofitModule
+
+import com.esrabildik.domain.repository.ProductRepository
+import com.esrabildik.domain.usecase.GetProductUseCase
+import com.esrabildik.domain.util.BASE_URL
+import com.esrabildik.remote.ProductApi
+import com.esrabildik.repository.ProductRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    //https://dummyjson.com/products
+    @Provides
+    fun provideProductApi() : ProductApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ProductApi::class.java)
+    }
+
+    @Provides
+    fun provideProductRepository(api : ProductApi) : ProductRepository {
+        return ProductRepositoryImpl(api)
+    }
+
+    @Provides
+    fun provideGetProductsUseCase(repository: ProductRepository) : GetProductUseCase {
+        return GetProductUseCase(repository)
+    }
+}
