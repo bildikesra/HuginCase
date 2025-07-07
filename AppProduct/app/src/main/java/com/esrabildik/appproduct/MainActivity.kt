1d8488838bccfe335917ec4d71bfe7ce140954a7
esrabildikk <esrabildik752@gmail.com>
2025-07-07 09:05:29 +0300

    feat : application class created

--- AppProduct/app/src/main/java/com/esrabildik/appproduct/MainActivity.kt
+++ AppProduct/app/src/main/java/com/esrabildik/appproduct/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.esrabildik.appproduct
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import com.esrabildik.appproduct.ui.theme.AppProductTheme
+import com.esrabildik.feature.screen.homeScreen.EmbeddedSearchBar
+import com.esrabildik.feature.screen.homeScreen.ProductScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,28 +26,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppProductTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    ProductScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppProductTheme {
-        Greeting("Android")
     }
 }
