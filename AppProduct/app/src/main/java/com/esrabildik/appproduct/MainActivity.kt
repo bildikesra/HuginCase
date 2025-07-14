97e77b60c12e95a404a155b7871653d5c58c6cb6
esrabildikk <esrabildik752@gmail.com>
2025-07-14 09:42:02 +0300

    feat : AppNavHost added

--- AppProduct/app/src/main/java/com/esrabildik/appproduct/MainActivity.kt
+++ AppProduct/app/src/main/java/com/esrabildik/appproduct/MainActivity.kt
@@ -5,30 +5,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import com.esrabildik.appproduct.ui.theme.AppProductTheme
-import com.esrabildik.feature.screen.homeScreen.EmbeddedSearchBar
-import com.esrabildik.feature.screen.homeScreen.ProductScreen
+import androidx.navigation.compose.rememberNavController
+import com.esrabildik.feature.ui.theme.AppProductTheme
+import com.esrabildik.feature.navigation.AppNavHost
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AppProductTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ProductScreen(modifier = Modifier.padding(innerPadding))
-                }
+                val navController = rememberNavController()
+                Scaffold(
+                    content = { innerPadding ->
+                        Box(modifier = Modifier.padding(innerPadding)) {
+                            AppNavHost(navController)
+                        }
+                    }
+                )
             }
         }
     }
 }
+
