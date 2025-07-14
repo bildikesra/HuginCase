34b43ef43d8610f5e1f8db250cc0550ed6f7dab0
esrabildikk <esrabildik752@gmail.com>
2025-07-14 09:40:17 +0300

    feat : ProductRepository created

--- AppProduct/domain/src/main/java/com/esrabildik/domain/repository/ProductRepository.kt
+++ AppProduct/domain/src/main/java/com/esrabildik/domain/repository/ProductRepository.kt
@@ -1,10 +1,13 @@
 package com.esrabildik.domain.repository
 
+
 import com.esrabildik.domain.model.Product
 import com.esrabildik.domain.util.APIResult
 import kotlinx.coroutines.flow.Flow
 
 interface ProductRepository {
-    fun getProducts() : Flow<APIResult<List<Product>>>
-    suspend fun getCategories() : Flow<APIResult<List<String>>>
+    fun getProducts(): Flow<APIResult<List<Product>>>
+    fun getCategories(): Flow<APIResult<List<String>>>
+    fun getProductById(id: String): Flow<APIResult<Product>>
+    fun getCategoryName(categoryName: String): Flow<APIResult<List<Product>>>
 }
